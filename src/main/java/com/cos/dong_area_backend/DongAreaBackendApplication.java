51395aadfb04cfcd1aca916f2f212c60661ef24a
성의근 <legeno09133@gmail.com>
2023-02-07 09:01:48 +0900

    Initial commit

---
+++ src/main/java/com/cos/dong_area_backend/DongAreaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cos.dong_area_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongAreaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongAreaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/dong_area_backend/DongAreaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.dong_area_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongAreaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
